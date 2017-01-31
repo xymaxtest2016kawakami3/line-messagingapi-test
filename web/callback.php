48f1c7034422b0bb9029b83e723c09dc2884d640
xymaxtest2016kawakami3 <xymaxtest2016+kawakami3@gmail.com>
2017-01-31 09:48:55 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -28,7 +28,7 @@ if ($type == "image" ) {
             [
               "type" => "message",
               "label" => "はい",
-              "text" => "はい"
+              "text" => "はい、そうです"
             ],
             [
               "type" => "message",
@@ -38,7 +38,7 @@ if ($type == "image" ) {
         ]
      ]
   ];
-} else if ($text == 'はい' ) {
+} else if ($text == 'はい、そうです' ) {
   $response_format_text = [
         "type" => "text",
         "text" => "対象の場所を教えて下さい。",
@@ -272,7 +272,7 @@ else if ($text == '修繕依頼・見積依頼') {
         ]
     ]
   ];
-} else if ($text == 'はい1' ) {
+} else if ($text == 'はい' ) {
   $response_format_text = [
         "type" => "text",
         "text" => "ザイマックスマート 溜池山王店 様ご登録ありがとうございます。\n続けてお名前をご入力下さい。",
